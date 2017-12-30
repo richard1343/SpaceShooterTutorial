67dcb2ebd43675d93da5120f8faf3f590305aaed
Richard.Kim <richard13431343@gmail.com>
2017-12-30 22:09:54 +0900

    Create Hazard
     -created Hazard

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+
+    }
+}
