53e0af5ba2f5418e56bfc44221e9a5f81a072dd6
Richard.Kim <richard13431343@gmail.com>
2017-12-30 21:36:47 +0900

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    Rigidbody rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Start ()
+    {
+        rb.velocity = transform.forward * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
